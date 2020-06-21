d80923599f20e91043edff9593775b106f3a2fe6
athroscf <fialloschris1@gmail.com>
2020-06-20 22:28:41 -0600

    Setting basic setup.

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Aux from '../Auxiliary/Auxiliary';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+
+export class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <main>
+                    
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+)
+
+ReactDOM.render( app, document.getElementById('root') );
 registerServiceWorker();
