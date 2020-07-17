2785d45c6a3c5dd580841c2f209fa62a1f28751c
athroscf <fialloschris1@gmail.com>
2020-07-17 17:49:14 -0600

    Disabling redux in production mode.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import pollsReducer from './store/reducers/polls';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : null;
 
 const rootReducer = combineReducers({
     polls: pollsReducer,
