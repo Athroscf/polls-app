5843409f858cd21a150402575be1f7746f84ec8f
athroscf <fialloschris1@gmail.com>
2020-07-17 22:24:31 -0600

    Creating test file for Home Component.

---
+++ src/containers/Home/Home.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Home } from './Home';
+import Button from '../../components/UI/Button/Button';
+
+configure({adapter: new Adapter()});
+
+describe('<Home />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Home />);
+    });
+
+    it('should render 1 <Button /> element if not authenticated', () => {
+        expect(wrapper.find(Button)).toHaveLength(1);
+    });
+
+    it('should render 2 <Button /> elements if authenticated', () => {
+        wrapper.setProps({isAuth: true});
+        expect(wrapper.find(Button)).toHaveLength(2);
+    });
+});
