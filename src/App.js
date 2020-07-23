b966ee03d056ada77a7c19be0258570c63c24662
Christopher Fiallos <Athroscf@users.noreply.github.com>
2020-07-23 16:55:28 -0600

    Revert "Merge branch 'develop'"
    This reverts commit 5a3bb6a1794cc9b83d96421292d33a54c16d213d, reversing
    changes made to a65343190080cfe05aaadf1b1a78d569298a0a78.

--- src/App.js
+++ src/App.js
@@ -1,74 +1,21 @@
-import React, { useEffect, Suspense } from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Layout from './hoc/Layout/Layout';
-import Home from './containers/Home/Home';
-import Logout from './containers/Auth/Logout/Logout';
-import * as actionTypes from './store/actions';
-
-const Polls = React.lazy(() => {
-  return import('./containers/Polls/Polls');
-});
-
-const Stats = React.lazy(() => {
-  return import('./containers/Stats/Stats');
-});
-
-const Auth = React.lazy(() => {
-  return import('./containers/Auth/Auth');
-});
-
-const app = props => {
-  const { onTryAutoSignUp } = props;
-
-  useEffect(() => {
-    onTryAutoSignUp();
-  }, [onTryAutoSignUp]);
-
-  let routes = (
-    <Switch>
-      <Route path="/polls" render={(props) => <Polls {...props}/>} />
-      <Route path="/auth" render={(props) => <Auth {...props}/>} />
-      <Route path="/" exact component={Home} />
-      <Redirect to="/" />
-    </Switch>
-  )
-
-  if ( props.isAuth ) {
-    routes = (
-      <Switch>
-          <Route path="/polls" render={(props) => <Polls {...props} />} />
-          <Route path="/stats" render={(props) => <Stats {...props} />} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={Home} />
-          <Redirect to="/" />
-      </Switch>
-    )
-  }
-
-  return (
-    <div>
-      <Layout email={props.email}>
-        <Suspense fallback={<p>Loading...</p>}>
-          {routes}
-        </Suspense>
-      </Layout>
-    </div>
-  );
-};
-
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email,
-    isAuth: state.auth.token !== null
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignUp: () => dispatch(actionTypes.authCheckState()),
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
+export default App;
