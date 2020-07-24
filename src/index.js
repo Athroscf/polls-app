cf7b689cd83b0f3619b78b6e92224cb43f04516c
Christopher Fiallos <Athroscf@users.noreply.github.com>
2020-07-23 19:34:45 -0600

    Revert "Merge branch 'develop'"
    This reverts commit 5a3bb6a1794cc9b83d96421292d33a54c16d213d, reversing
    changes made to a65343190080cfe05aaadf1b1a78d569298a0a78.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { StylesProvider } from '@material-ui/core/styles';
-import thunk from 'redux-thunk';
-
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import pollsReducer from './store/reducers/polls';
-import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : null;
-
-const rootReducer = combineReducers({
-    polls: pollsReducer,
-    auth: authReducer
-})
-
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
-
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <StylesProvider injectFirst>
-                <App/>
-            </StylesProvider>
-        </BrowserRouter>
-    </Provider>
-)
-
-ReactDOM.render( app, document.getElementById('root') );
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
